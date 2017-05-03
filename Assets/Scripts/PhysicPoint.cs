f6a38283c21611429cd01677f4e2a394e3e98875
KemiLand <mikymike95@gmail.com>
2017-05-03 22:44:29 +0200

    First commit, bugged and not really working

---
+++ Assets/Scripts/PhysicPoint.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhysicPoint : MonoBehaviour {
+    Rigidbody2D m_Body;
+    float wantedVelocity = 3.0f * Mathf.PI;
+    [SerializeField]
+    float ray = 0.0f;
+    [SerializeField]
+    float velocity;
+	// Use this for initialization
+	void Start () {
+        m_Body = GetComponent<Rigidbody2D>();
+        //m_Body.velocity = new Vector2(0.0f, wantedVelocity);
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        ray = transform.position.magnitude;
+        m_Body.AddForce(Mathf.Pow(wantedVelocity, 2.0f) / ray * (-transform.position.normalized));
+        
+        velocity = m_Body.velocity.magnitude;
+    }
+}
---
+++ Assets/Scripts/TransformPoint.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransformPoint : MonoBehaviour {
+
+    [SerializeField] Transform bluePlanet;
+    [SerializeField] Transform sun;
+    [SerializeField] Rigidbody2D m_BlueBody;
+    [SerializeField] Rigidbody2D m_SunBody;
+    [SerializeField] Vector3 wantedForce;
+    [SerializeField] Vector3 distance;
+    [SerializeField] Vector3 acceleration;
+    [SerializeField] Vector3 velocity;
+    float wantedVelocity = 3.0f * Mathf.PI;
+    float ray;
+
+    //
+    //Need demo, j'arrive pas a grand chose
+    //
+
+    void Start()
+    {
+        m_BlueBody = GetComponent<Rigidbody2D>();
+        //m_BlueBody.velocity = new Vector2(0.0f, wantedVelocity);
+    }
+
+    void FixedUpdate ()
+    {
+        ray = transform.position.magnitude;
+        distance = sun.transform.position - bluePlanet.transform.position;
+        wantedForce = distance * (m_SunBody.mass * m_BlueBody.mass / (transform.position - sun.transform.position).magnitude);
+
+        //Pas trop sûr de l'utilisation d'Addforce, mes forces donnent des trajectoires bizarres
+        //m_Body.AddForce(Mathf.Pow(wantedForce, 2.0f) / ray * (-transform.position.normalized));
+
+        acceleration = wantedForce / m_BlueBody.mass;
+        velocity = acceleration * Time.deltaTime + velocity;
+        transform.position += Time.deltaTime * velocity;
+    }
+}
